870901da030065b97a25fb0b474ad69a872fb40b
NatasaVasicRA10 <vasic.ra10.2018@uns.ac.rs>
2022-10-24 18:15:02 +0200

    initial backend

---
+++ projectIsa/projectIsa/src/main/java/com/example/projectIsa/ProjectIsaApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectIsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectIsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectIsaApplication.class, args);
+	}
+
+}
---
+++ projectIsa/projectIsa/src/test/java/com/example/projectIsa/ProjectIsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectIsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectIsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
