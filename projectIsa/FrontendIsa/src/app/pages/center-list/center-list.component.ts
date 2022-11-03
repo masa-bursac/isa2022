72b70a156911576e68fa5dbc290eff512b765843
NatasaVasicRA10 <vasic.ra10.2018@uns.ac.rs>
2022-11-03 18:59:19 +0100

    feat: #3.14 filter centers

--- projectIsa/FrontendIsa/src/app/pages/center-list/center-list.component.ts
+++ projectIsa/FrontendIsa/src/app/pages/center-list/center-list.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { CenterService } from 'src/app/services/center.service';
 
+interface Rating {
+  value: number;
+  viewValue: string;
+}
+
+interface City {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-center-list',
   templateUrl: './center-list.component.html',
@@ -9,8 +19,9 @@ import { CenterService } from 'src/app/services/center.service';
 export class CenterListComponent implements OnInit {
 
   public allCenters: any[] = [];
-  public searchedCenters: any[]= [];
-  search : string ='';
+  public searchedCenters: any[] = [];
+  public allFiltered: any[] = [];
+  search : string = '';
 
   constructor(private centerService : CenterService) { }
 
@@ -24,11 +35,32 @@ export class CenterListComponent implements OnInit {
     });
   }
 
-  public Search(): void {
+  public searchCenters(): void {
     this.centerService.searchCenters(this.search).subscribe(data => {
       this.searchedCenters = data;
     }
     );
+  }
+
+  ratings: Rating[] = [
+    {value: 5, viewValue: '5'},
+    {value: 4, viewValue: '4'},
+    {value: 3, viewValue: '3'},
+    {value: 2, viewValue: '2'},
+    {value: 1, viewValue: '1'}
+  ];
+
+  citys: City[] = [
+    {value: 'Novi Sad', viewValue: 'Novi Sad'},
+    {value: 'Beograd', viewValue: 'Beograd'}
+  ];
+
+  public filterRating(value: number): void {
+    this.allFiltered = this.allCenters.filter(center => center.rating === value);
+  }
+
+  public filterCity(value: string): void {
+    this.allFiltered = this.allCenters.filter(center => center.city === value);
   }
 
 }
