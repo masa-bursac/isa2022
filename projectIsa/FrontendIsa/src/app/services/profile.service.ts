1bf827ecd1df8ca94ef0889907c15010df932910
masa.bursac <bursac.ra133.2018@uns.ac.rs>
2022-11-14 12:07:34 +0100

    fix: merge error

--- projectIsa/FrontendIsa/src/app/services/profile.service.ts
+++ projectIsa/FrontendIsa/src/app/services/profile.service.ts
@@ -21,6 +21,7 @@ export class ProfileService {
   
   public changeCenterAdminPassword(body: any): Observable<any>{
     return this.http.put(profile_url+`/changeCenterAdminPassword`, body);
+  }
 
   public getAllUsers(): Observable<any>{
     return this.http.get(profile_url+`/getUsers`);
